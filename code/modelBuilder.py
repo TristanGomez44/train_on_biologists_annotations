3292ca4e48495b11cd155c4e21f227a7a2254468
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2019-10-17 09:35:34 +0200

    Added resnet3D

--- code/modelBuilder.py
+++ code/modelBuilder.py
@@ -3,8 +3,10 @@ from torch import nn
 from torch.nn import functional as F
 from torch.nn import DataParallel
 import resnet
+import resnet3D
 
 import args
+import sys
 
 def buildFeatModel(featModelName):
     ''' Build a visual feature model
@@ -15,8 +17,12 @@ def buildFeatModel(featModelName):
     - featModel (nn.Module): the visual feature extractor
 
     '''
-
-    featModel = getattr(resnet,featModelName)(pretrained=True)
+    if featModelName.find("resnet") != -1:
+        featModel = getattr(resnet,featModelName)(pretrained=True)
+    elif featModelName == "r2plus1d_18":
+        featModel = getattr(resnet3D,featModelName)(pretrained=True)
+    else:
+        raise ValueError("Unknown model type : ",featModelName)
 
     return featModel
 
@@ -63,8 +69,37 @@ class CNN(nn.Module):
         # N x T x classNb
         return x
 
+class CNN3D(CNN):
+
+    def __init__(self,featModelName,dropout=0.5,classNb=16):
+
+        super(CNN3D,self).__init__(featModelName,dropout,classNb)
+
+        #The r2plus1d_18 architecture has 512 features
+        self.nbFeat = 512
+
+        self.linLay = nn.Linear(self.nbFeat,classNb)
+
+    def computeFeat(self,x):
+        # N x T x C x H x L
+        self.batchSize = x.size(0)
+        x = x.permute(0,2,1,3,4)
+        # N x C x T x H x L
+        x = self.featMod(x)
+        # N x D x T
+        x = x.permute(0,2,1)
+        # N x T x D
+        x = x.contiguous().view(x.size(0)*x.size(1),-1)
+        # NT x D
+        return x
+
 def netBuilder(args):
-    net = CNN(args.feat,args.dropout,args.class_nb)
+    if args.feat.find("resnet") != -1:
+        net = CNN(args.feat,args.dropout,args.class_nb)
+    elif args.feat == "r2plus1d_18":
+        net = CNN3D(args.feat,args.dropout,args.class_nb)
+    else:
+        raise ValueError("Unknown model type : ",args.feat)
 
     return net
 
