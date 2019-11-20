709089751d5277bfae37de6cc920d7cd020854da
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2019-11-20 12:40:22 +0100

    Added script for cross validation experiment

---
+++ code/agregate_cross_validation.sh
@@ -0,0 +1,44 @@
+
+split_by () {
+    string=$1
+    separator=$2
+
+    tmp=${string//"$separator"/$'\2'}
+    IFS=$'\2' read -a arr <<< "$tmp"
+    for substr in "${arr[@]}" ; do
+        echo "$substr"
+    done
+    echo
+}
+
+extractEpoch() {
+	filename=$(basename $1)
+	epoch="$(split_by $filename epoch)"
+  epoch=($epoch)
+	echo ${epoch[1]}
+}
+
+getBestPath() {
+	best_path=$(ls ../models/cross_val/model$1*best*)
+	best_path=($best_path)
+	best_path=${best_path[0]}
+	echo $best_path
+}
+
+modelIdList=()
+#epochList=()
+
+for initFile in ../models/cross_val/*.ini
+do
+  model_id=$(basename $initFile .ini)
+  #bestPath=$(getBestPath $model_id)
+  #epoch=$(extractEpoch $bestPath)
+  modelIdList+=($model_id)
+  #epochList+=($epoch)
+
+done
+
+python processResults.py --eval_model --exp_id cross_val --param_agr feat temp_mod score_conv_attention \
+                         --names ResNet ResNet3D ResNet-LSTM ResNet-SC ResNet-SA \
+                         --keys resnet18,linear,False r2plus1d_18,linear,False resnet18,lstm,False resnet18,score_conv,False resnet18,score_conv,True \
+                         --epochs_to_process 1 1 1 1 1 1 1 1 1 1 --model_ids ${modelIdList[@]}
---
+++ code/run_exp.sh
@@ -0,0 +1,5 @@
+
+#./cross_validation.sh 5 0 2 2 2 model_smallgpu.config
+#./cross_validation.sh 5 1 2 2 2 model_smallgpu.config
+
+./agregate_cross_validation.sh
