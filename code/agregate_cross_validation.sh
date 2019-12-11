58ac63740452439a39b4629d0a0dd7a1c2399a57
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2019-12-11 18:16:29 +0100

    Fixed experiment scripts

--- code/agregate_cross_validation.sh
+++ code/agregate_cross_validation.sh
@@ -19,26 +19,32 @@ extractEpoch() {
 }
 
 getBestPath() {
-	best_path=$(ls ../models/cross_val/model$1*best*)
+	best_path=$(ls ../models/cross_val2/model$1*best*)
 	best_path=($best_path)
 	best_path=${best_path[0]}
 	echo $best_path
 }
 
 modelIdList=()
-#epochList=()
+epochList=()
 
-for initFile in ../models/cross_val/*.ini
+for initFile in ../models/cross_val2/*.ini
 do
   model_id=$(basename $initFile .ini)
-  #bestPath=$(getBestPath $model_id)
-  #epoch=$(extractEpoch $bestPath)
+  bestPath=$(getBestPath $model_id)
+  epoch=$(extractEpoch $bestPath)
   modelIdList+=($model_id)
-  #epochList+=($epoch)
+  epochList+=($epoch)
 
 done
 
-python processResults.py --eval_model --exp_id cross_val --param_agr feat temp_mod score_conv_attention \
-                         --names ResNet ResNet3D ResNet-LSTM ResNet-SC ResNet-SA \
-                         --keys resnet18,linear,False r2plus1d_18,linear,False resnet18,lstm,False resnet18,score_conv,False resnet18,score_conv,True \
-                         --epochs_to_process 1 1 1 1 1 1 1 1 1 1 --model_ids ${modelIdList[@]}
+
+#python processResults.py --eval_model --exp_id cross_val2 --param_agr feat temp_mod score_conv_attention \
+#                         --names ResNet ResNet3D ResNet-LSTM ResNet-SC ResNet-SA \
+#                         --keys resnet18,linear,False r2plus1d_18,linear,False resnet18,lstm,False resnet18,score_conv,False resnet18,score_conv,True \
+#                         --epochs_to_process 1 1 1 1 1 1 1 1 1 1 --model_ids ${modelIdList[@]}
+
+python processResults.py --eval_model --exp_id cross_val2 --param_agr feat temp_mod score_conv_attention \
+                        --names ResNet ResNet-LSTM ResNet-SC ResNet-SA \
+                        --keys resnet18,linear,False resnet18,lstm,False resnet18,score_conv,False resnet18,score_conv,True \
+                        --epochs_to_process ${epochList[@]} --model_ids ${modelIdList[@]}
--- code/run_exp.sh
+++ code/run_exp.sh
@@ -1,5 +1,5 @@
 
-#./cross_validation.sh 5 0 2 2 2 model_smallgpu.config
-#./cross_validation.sh 5 1 2 2 2 model_smallgpu.config
+./cross_validation.sh 5 0 80 10 10 model_smallgpu.config
+./cross_validation.sh 5 1 80 10 10 model_smallgpu.config
 
 ./agregate_cross_validation.sh
