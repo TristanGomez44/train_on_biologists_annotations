870670063902c4f1b5f148bfcf199cb9ba0a501c
Remi VALLEE <remi.vallee@etu.univ-nantes.fr>
2020-03-09 16:55:55 +0100

    Added new loader that does not cut too big part of image

--- code/load_data.py
+++ code/load_data.py
@@ -15,6 +15,7 @@ import utils
 import torch.distributed as dist
 from random import Random
 
+
 class Partition(object):
 
     def __init__(self, data, index):
@@ -27,6 +28,7 @@ class Partition(object):
     def __getitem__(self, index):
         data_idx = self.index[index]
         return self.data[data_idx]
+
 
 class DataPartitioner(object):
 
@@ -47,16 +49,22 @@ class DataPartitioner(object):
     def use(self, partition):
         return Partition(self.data, self.partitions[partition])
 
+
 def buildTrainLoader(args):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-    transf = transforms.Compose([transforms.RandomResizedCrop(224),transforms.RandomHorizontalFlip(),transforms.ToTensor(),normalize])
+    if args.old_preprocess:
+        transf = transforms.Compose(
+            [transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize])
+    else:
+        transf = transforms.Compose([transforms.Resize(224), transforms.RandomCrop(224, padding=0, pad_if_needed=True),
+                                     transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize])
 
-    train_dataset = torchvision.datasets.ImageFolder("../data/{}".format(args.dataset_train),transf)
+    train_dataset = torchvision.datasets.ImageFolder("../data/{}".format(args.dataset_train), transf)
     totalLength = len(train_dataset)
 
     if args.prop_set_int_fmt:
-        train_prop = args.train_prop/100
+        train_prop = args.train_prop / 100
     else:
         train_prop = args.train_prop
 
@@ -65,9 +73,10 @@ def buildTrainLoader(args):
     if args.cuda:
         torch.cuda.manual_seed(1)
 
-    train_dataset,_ = torch.utils.data.random_split(train_dataset, [int(totalLength*train_prop),totalLength-int(totalLength*train_prop)])
+    train_dataset, _ = torch.utils.data.random_split(train_dataset, [int(totalLength * train_prop),
+                                                                     totalLength - int(totalLength * train_prop)])
 
-    kwargs = {"shuffle":True}
+    kwargs = {"shuffle": True}
 
     if args.distributed:
         size = dist.get_world_size()
@@ -78,22 +87,26 @@ def buildTrainLoader(args):
     else:
         bsz = args.batch_size
 
-    trainLoader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=bsz, # use custom collate function here
-                      pin_memory=False,num_workers=args.num_workers,**kwargs)
+    trainLoader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bsz,  # use custom collate function here
+                                              pin_memory=False, num_workers=args.num_workers, **kwargs)
 
-    return trainLoader,train_dataset
+    return trainLoader, train_dataset
 
-def buildTestLoader(args,mode,normalize=True):
 
-    datasetName = getattr(args,"dataset_{}".format(mode))
+def buildTestLoader(args, mode, normalize=True):
+    datasetName = getattr(args, "dataset_{}".format(mode))
+
+    if args.old_preprocess:
+        transf = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()])
+    else:
+        transf = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor()])
 
     if normalize:
-        normalizeFunc = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-        transf = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224),transforms.ToTensor(),normalizeFunc])
-    else:
-        transf = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224),transforms.ToTensor()])
+        normalizeFunc = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transf = transforms.Compose([transf, normalizeFunc])
 
-    test_dataset = torchvision.datasets.ImageFolder("../data/{}".format(datasetName),transf)
+
+    test_dataset = torchvision.datasets.ImageFolder("../data/{}".format(datasetName), transf)
 
     if mode == "val" and args.dataset_train == args.dataset_val:
         np.random.seed(1)
@@ -102,87 +115,90 @@ def buildTestLoader(args,mode,normalize=True):
             torch.cuda.manual_seed(1)
 
         if args.prop_set_int_fmt:
-            train_prop = args.train_prop/100
+            train_prop = args.train_prop / 100
         else:
             train_prop = args.train_prop
 
         totalLength = len(test_dataset)
-        _,test_dataset = torch.utils.data.random_split(test_dataset, [int(totalLength*train_prop),totalLength-int(totalLength*train_prop)])
+        _, test_dataset = torch.utils.data.random_split(test_dataset, [int(totalLength * train_prop),
+                                                                       totalLength - int(totalLength * train_prop)])
 
         np.random.seed(args.seed)
         torch.manual_seed(args.seed)
         if args.cuda:
             torch.cuda.manual_seed(args.seed)
 
-    testLoader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=args.val_batch_size,num_workers=args.num_workers)
+    testLoader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=args.val_batch_size,
+                                             num_workers=args.num_workers)
 
     return testLoader
 
+
 def addArgs(argreader):
-
     argreader.parser.add_argument('--pretrain_dataset', type=str, metavar='N',
-                        help='The network producing the features can only be pretrained on \'imageNet\'. This argument must be \
+                                  help='The network producing the features can only be pretrained on \'imageNet\'. This argument must be \
                             set to \'imageNet\' datasets.')
-    argreader.parser.add_argument('--batch_size', type=int,metavar='BS',
-                        help='The batchsize to use for training')
-    argreader.parser.add_argument('--val_batch_size', type=int,metavar='BS',
-                        help='The batchsize to use for validation')
+    argreader.parser.add_argument('--batch_size', type=int, metavar='BS',
+                                  help='The batchsize to use for training')
+    argreader.parser.add_argument('--val_batch_size', type=int, metavar='BS',
+                                  help='The batchsize to use for validation')
 
-    argreader.parser.add_argument('--tr_len', type=int,metavar='LMAX',
-                        help='The maximum length of a training sequence')
-    argreader.parser.add_argument('--val_l', type=int,metavar='LMAX',
-                        help='Length of sequences for validation.')
+    argreader.parser.add_argument('--tr_len', type=int, metavar='LMAX',
+                                  help='The maximum length of a training sequence')
+    argreader.parser.add_argument('--val_l', type=int, metavar='LMAX',
+                                  help='Length of sequences for validation.')
 
-    argreader.parser.add_argument('--img_size', type=int,metavar='EDGE_SIZE',
-                        help='The size of each edge of the images after resizing, if resizing is desired. Else is should be equal to --orig_img_size')
-    argreader.parser.add_argument('--orig_img_size', type=int,metavar='EDGE_SIZE',
-                        help='The size of each edge of the images before preprocessing.')
+    argreader.parser.add_argument('--img_size', type=int, metavar='EDGE_SIZE',
+                                  help='The size of each edge of the images after resizing, if resizing is desired. Else is should be equal to --orig_img_size')
+    argreader.parser.add_argument('--orig_img_size', type=int, metavar='EDGE_SIZE',
+                                  help='The size of each edge of the images before preprocessing.')
 
-    argreader.parser.add_argument('--train_prop', type=float,metavar='END',
-                        help='The proportion of the train dataset to use for training when working in non video mode. The rest will be used for validation.')
+    argreader.parser.add_argument('--train_prop', type=float, metavar='END',
+                                  help='The proportion of the train dataset to use for training when working in non video mode. The rest will be used for validation.')
 
-    argreader.parser.add_argument('--prop_set_int_fmt', type=args.str2bool,metavar='BOOL',
-                        help='Set to True to set the sets (train, validation and test) proportions\
+    argreader.parser.add_argument('--prop_set_int_fmt', type=args.str2bool, metavar='BOOL',
+                                  help='Set to True to set the sets (train, validation and test) proportions\
                             using int between 0 and 100 instead of float between 0 and 1.')
 
-
-    argreader.parser.add_argument('--dataset_train', type=str,metavar='DATASET',
-                        help='The dataset for training. Can be "big" or "small"')
-    argreader.parser.add_argument('--dataset_val', type=str,metavar='DATASET',
-                        help='The dataset for validation. Can be "big" or "small"')
-    argreader.parser.add_argument('--dataset_test', type=str,metavar='DATASET',
-                        help='The dataset for testing. Can be "big" or "small"')
+    argreader.parser.add_argument('--dataset_train', type=str, metavar='DATASET',
+                                  help='The dataset for training. Can be "big" or "small"')
+    argreader.parser.add_argument('--dataset_val', type=str, metavar='DATASET',
+                                  help='The dataset for validation. Can be "big" or "small"')
+    argreader.parser.add_argument('--dataset_test', type=str, metavar='DATASET',
+                                  help='The dataset for testing. Can be "big" or "small"')
 
     argreader.parser.add_argument('--resize_image', type=args.str2bool, metavar='S',
-                        help='to resize the image to the size indicated by the img_width and img_heigth arguments.')
+                                  help='to resize the image to the size indicated by the img_width and img_heigth arguments.')
 
     argreader.parser.add_argument('--class_nb', type=int, metavar='S',
-                        help='The number of class of to model')
+                                  help='The number of class of to model')
 
     argreader.parser.add_argument('--augment_data', type=args.str2bool, metavar='S',
-                        help='Set to True to augment the training data with transformations')
+                                  help='Set to True to augment the training data with transformations')
 
     argreader.parser.add_argument('--mask_time_on_image', type=args.str2bool, metavar='S',
-                        help='To mask the time displayed on the images')
+                                  help='To mask the time displayed on the images')
 
     argreader.parser.add_argument('--min_phase_nb', type=int, metavar='S',
-                        help='The minimum number of phases a video must have to be included in the dataset')
+                                  help='The minimum number of phases a video must have to be included in the dataset')
 
     argreader.parser.add_argument('--grid_shuffle', type=args.str2bool, metavar='S',
-                        help='Apply a grid shuffle transformation from albumentation to the training images')
+                                  help='Apply a grid shuffle transformation from albumentation to the training images')
 
     argreader.parser.add_argument('--grid_shuffle_size', type=int, metavar='S',
-                        help='The grid size for grid shuffle.')
-
+                                  help='The grid size for grid shuffle.')
 
     argreader.parser.add_argument('--grid_shuffle_test', type=args.str2bool, metavar='S',
-                        help='Apply a grid shuffle transformation from albumentation to the testing images')
+                                  help='Apply a grid shuffle transformation from albumentation to the testing images')
 
     argreader.parser.add_argument('--grid_shuffle_test_size', type=int, metavar='S',
-                        help='The grid size for grid shuffle for the test phase.')
+                                  help='The grid size for grid shuffle for the test phase.')
 
     argreader.parser.add_argument('--sobel', type=args.str2bool, metavar='S',
-                        help='To apply sobel transform to each image')
+                                  help='To apply sobel transform to each image')
+
+    argreader.parser.add_argument('--old_preprocess', type=args.str2bool, metavar='S',
+                                  help='To use the old images pre-processor.')
 
 
     return argreader
