Fixed missing minus sign on loss function

code/modelBuilder.py
@@ -389,7 +389,7 @@ class TopkPointExtractor(nn.Module):
         if self.topk_euclinorm:
             x = torch.pow(pointFeaturesMap, 2).sum(dim=1, keepdim=True)
         elif self.textEncod:
-            x = computeTotalDiff(featureMaps,dilation=1)
+            x = -computeTotalSim(featureMaps,dilation=1)
             retDict["featVolume"] = featureMaps
         elif self.hasLinearProb:
             x = torch.sigmoid(self.linearProb(pointFeaturesMap))
@@ -526,7 +526,7 @@ def applyDiffKer_CosSimi(where,features,dilation=1):
 
     return diff
 
-def computeTotalDiff(features,dilation):
+def computeTotalSim(features,dilation):
     topDiff = applyDiffKer_CosSimi("top",features,dilation)
     botDiff = applyDiffKer_CosSimi("bot",features,dilation)
     leftDiff = applyDiffKer_CosSimi("left",features,dilation)

code/trainVal.py
@@ -167,10 +167,10 @@ def text_enc_term(text_enc_weight, resDict, data, target,posDil,negDil,margin):
 
     features = resDict["featVolume"]
 
-    totalDiffPos = modelBuilder.computeTotalDiff(features,posDil)
-    totalDiffNeg = modelBuilder.computeTotalDiff(features,negDil)
+    totalSimPos = modelBuilder.computeTotalSim(features,posDil)
+    totalSimNeg = modelBuilder.computeTotalSim(features,negDil)
 
-    return -text_enc_weight*torch.max(totalDiffNeg.mean() - totalDiffPos.mean()+margin,0)[0]
+    return -text_enc_weight*torch.max(totalSimPos.mean() - totalSimNeg.mean()+margin,0)[0]
 
 def average_gradients(model):
     size = float(dist.get_world_size())