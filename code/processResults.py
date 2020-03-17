ea0beeba1346743c584b0f7a94ec96daa1e1f432
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-03-17 15:26:32 +0100

    Added function to plot prob maps

--- code/processResults.py
+++ code/processResults.py
@@ -22,7 +22,6 @@ import load_data
 
 import metrics
 import utils
-import trainVal
 import scipy
 import sys
 
@@ -39,6 +38,10 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+from scipy.signal import argrelextrema
+import scipy.ndimage.filters as filters
+import scipy.ndimage.morphology as morphology
+
 def plotPointsImageDataset(imgNb,redFact,plotDepth,args):
 
     cm = plt.get_cmap('plasma')
@@ -51,7 +54,7 @@ def plotPointsImageDataset(imgNb,redFact,plotDepth,args):
     points = np.concatenate(list(map(lambda x:np.load(x)[:imgNb][:,:,:][np.newaxis],pointPaths)),axis=0)
     points = np.transpose(points, axes=[1,0,2,3])
 
-    imgLoader = load_data.buildSeqTestLoader(args,"val",normalize=False)
+    imgLoader = load_data.buildTestLoader(args,"val",normalize=False)
 
     batchNb = imgNb//args.val_batch_size
     totalImgNb = 0
@@ -95,6 +98,67 @@ def plotPointsImageDataset(imgNb,redFact,plotDepth,args):
         if batchInd>=batchNb:
             break
 
+def plotProbMaps(imgNb,args,norm=False):
+
+    exp_id = args.exp_id
+    model_id = args.model_id
+
+    probMapPaths = sorted(glob.glob("../results/{}/prob_map_{}_epoch*.npy".format(exp_id,model_id)),key=utils.findNumbers)
+    cm = plt.get_cmap('plasma')
+
+    probmaps = np.concatenate(list(map(lambda x:np.load(x)[:imgNb][:,:,:][np.newaxis],probMapPaths)),axis=0)
+
+    imgLoader = load_data.buildTestLoader(args,"val",normalize=False)
+
+    batchNb = imgNb//args.val_batch_size
+    totalImgNb = 0
+
+    for batchInd,(imgBatch,_) in enumerate(imgLoader):
+        for imgInd in range(len(imgBatch)):
+
+            if totalImgNb<imgNb:
+                print("\t",imgInd,"/",totalImgNb)
+                print("\t","Writing video",imgInd)
+                with imageio.get_writer("../vis/{}/probmap_{}_img{}.mp4".format(exp_id,model_id,totalImgNb), mode='I',fps=20,quality=9) as writer:
+
+                    img = imgBatch[imgInd].detach().permute(1,2,0).numpy().astype(float)
+
+                    for epoch in range(len(probmaps)):
+                        dest = Image.new('RGB', (img.shape[1]*2,img.shape[0]))
+                        imgPIL = Image.fromarray((255*img).astype("uint8"))
+                        dest.paste(imgPIL, (0,0))
+
+                        probmap = probmaps[epoch,imgInd]
+
+                        if norm:
+                            probmap = (probmap-probmap.min())/(probmap.max()-probmap.min())
+                            probmap *= 255
+
+                        if args.pn_topk:
+                            horizontPadd = np.zeros((probmap.shape[0],3,probmap.shape[2]))
+                            probmap = np.concatenate((probmap,horizontPadd),axis=1)
+                            probmap = np.concatenate((horizontPadd,probmap),axis=1)
+
+                            verticaPadd = np.zeros((probmap.shape[0],probmap.shape[1],3))
+                            probmap = np.concatenate((probmap,verticaPadd),axis=2)
+                            probmap = np.concatenate((verticaPadd,probmap),axis=2)
+
+                        probmap = resize(probmap[0], (img.shape[0],img.shape[1]),anti_aliasing=True,mode="constant",order=0)
+
+                        probmapPIL = Image.fromarray(probmap.astype("uint8"))
+
+                        dest.paste(probmapPIL, (img.shape[1],0))
+
+                        draw = ImageDraw.Draw(dest)
+                        draw.text((0, 0),str(epoch),(0,0,0))
+                        dest = np.asarray(dest)
+                        cv2.imwrite("testProcessResults.png",dest)
+                        writer.append_data(img_as_ubyte(dest.astype("uint8")))
+
+                    totalImgNb += 1
+        if batchInd>=batchNb:
+            break
+
 def main(argv=None):
 
     #Getting arguments from config file and command line
@@ -106,7 +170,10 @@ def main(argv=None):
     argreader.parser.add_argument('--plot_points_image_dataset',action="store_true",help='To plot the points computed by the visual module of a point net model \
                                     on an image dataset. The -c (config file), --image_nb and --reduction_fact arg must be set.')
 
-    argreader.parser.add_argument('--image_nb',type=int,metavar="INT",help='For the --plot_points_image_dataset arg. \
+    argreader.parser.add_argument('--plot_prob_maps',action="store_true",help='To plot the points computed by the visual module of a point net model \
+                                    on an image dataset. The -c (config file), --image_nb arg must be set.')
+
+    argreader.parser.add_argument('--image_nb',type=int,metavar="INT",help='For the --plot_points_image_dataset and the --plot_prob_maps args. \
                                     The number of images to plot the points of.')
 
     argreader.parser.add_argument('--reduction_fact',type=int,metavar="INT",help='For the --plot_points_image_dataset arg.\
@@ -114,6 +181,7 @@ def main(argv=None):
                                     224x224 and the points cloud lies in a 56x56 frame, this arg should be 224/56=4')
 
     argreader.parser.add_argument('--plot_depth',type=str2bool,metavar="BOOL",help='For the --plot_points_image_dataset arg. Plots the depth instead of point feature norm.')
+    argreader.parser.add_argument('--norm',type=str2bool,metavar="BOOL",help='For the --plot_prob_maps arg. Normalise each prob map.')
 
     argreader = load_data.addArgs(argreader)
 
@@ -125,6 +193,8 @@ def main(argv=None):
 
     if args.plot_points_image_dataset:
         plotPointsImageDataset(args.image_nb,args.reduction_fact,args.plot_depth,args)
+    if args.plot_prob_maps:
+        plotProbMaps(args.image_nb,args,args.norm)
 
 if __name__ == "__main__":
     main()
