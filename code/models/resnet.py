24fb0acf6455f4be51e49be3372d2ae48cb71691
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-09-28 19:34:16 +0200

    Added circular padding

--- code/models/resnet.py
+++ code/models/resnet.py
@@ -34,7 +34,7 @@ model_urls = {
 def conv3x3(in_planes, out_planes, stride=1,dilation=1,groups=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, bias=False,dilation=dilation,groups=groups)
+                     padding=dilation, bias=False,dilation=dilation,groups=groups,padding_mode="circular")
 
 
 def conv1x1(in_planes, out_planes, stride=1,groups=1):
