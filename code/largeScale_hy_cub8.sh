0ab9b09732953109ce8f39986bce4729c28f5bcb
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-10-07 00:09:17 +0200

    Added more batch size values and different batch size/learning rate pairs

--- code/largeScale_hy_cub8.sh
+++ code/largeScale_hy_cub8.sh
@@ -1,39 +1,60 @@
 case $1 in
   "r18")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r18 --epochs 300   --first_mod resnet18   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r18 --first_mod resnet18   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
     ;;
   "r34")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r34 --epochs 300   --first_mod resnet34   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r34 --first_mod resnet34   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
     ;;
   "r101")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r101 --epochs 300   --first_mod resnet101 --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id r101 --first_mod resnet101 --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False
     ;;
   "b3")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b3 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 3
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b3 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 3
     ;;
   "b6")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b6 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 6
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b6 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 6
     ;;
   "b24")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b24 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 24
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b24 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 24
+    ;;
+  "b24-lr0.004")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b24-lr0.004 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 24 --lr 0.004
+    ;;
+  "b12-lr0.002")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b12-lr0.002 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 12 --lr 0.002
+    ;;
+  "b48-lr0.008")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b48-lr0.008 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 48 --lr 0.008
+    ;;
+  "b24-lr0.0005")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b24-lr0.0005 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 24 --lr 0.0005
+    ;;
+  "b33")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b33 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 33
+    ;;
+  "b16")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b16 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 16
+    ;;
+  "b20")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b20 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 20
     ;;
   "b48")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b48 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 48
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b48 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 48
     ;;
   "lr0.004")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.004 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.004
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.004 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.004
     ;;
   "lr0.002")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.002 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.002
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.002 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.002
     ;;
   "lr0.0005")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.0005 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.0005
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.0005 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.0005
     ;;
   "lr0.00025")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.00025 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.00025
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.00025 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.00025
     ;;
   "lr0.000125")
-    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.000125 --epochs 300   --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.000125
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id lr0.000125 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --lr 0.000125
     ;;
   "*")
     echo "no such model"
