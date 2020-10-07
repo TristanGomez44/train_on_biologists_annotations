3c8fee9e114172944556b67c3dbd9107a7a85beb
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-10-07 11:01:09 +0200

    Kept exploring the ratio b24/b20 to lr0.004

--- code/largeScale_hy_cub8.sh
+++ code/largeScale_hy_cub8.sh
@@ -26,6 +26,9 @@ case $1 in
   "b48-lr0.008")
     python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b48-lr0.008 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 48 --lr 0.008
     ;;
+  "b72-lr0.012")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b72-lr0.012 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 72 --lr 0.012
+    ;;
   "b24-lr0.0005")
     python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b24-lr0.0005 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 24 --lr 0.0005
     ;;
@@ -37,6 +40,15 @@ case $1 in
     ;;
   "b20")
     python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b20 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 20
+    ;;
+  "b20-lr0.004")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b20-lr0.004 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 20 --lr 0.004
+    ;;
+  "b40-lr0.008")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b40-lr0.008 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 40 --lr 0.008
+    ;;
+  "b60-lr0.012")
+    python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b60-lr0.012 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 60 --lr 0.012
     ;;
   "b48")
     python trainVal.py -c model_cub8.config --exp_id CUB8_HYP --model_id b48 --first_mod resnet50   --resnet_bilinear True --bil_cluster True --bil_cluster_ensemble False --batch_size 48
