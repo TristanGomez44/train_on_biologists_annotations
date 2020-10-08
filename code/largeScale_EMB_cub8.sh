edecb263530773a9c899d24bdcd9087077a39dc6
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-10-08 17:02:34 +0200

    Reduced max epoch

--- code/largeScale_EMB_cub8.sh
+++ code/largeScale_EMB_cub8.sh
@@ -1,8 +1,8 @@
 case $1 in
   "clusCatSoftm")
     python trainVal.py -c model_cub8.config --dataset_train embryo_img_train --dataset_val embryo_img_train --dataset_test embryo_img_test \
-                          --exp_id EMB8 --model_id bilClusCat_softm_hyp --epochs 300      --resnet_bilinear True --bil_cluster True  \
-                          --val_batch_size 25 --apply_softmax_on_sim True --softm_coeff 1 --shufle_test_set True
+                          --exp_id EMB8 --model_id bilClusCat_softm_hyp --epochs 160      --resnet_bilinear True --bil_cluster True  \
+                          --val_batch_size 25 --apply_softmax_on_sim True --softm_coeff 1 --shuffle_test_set True
     ;;
   "*")
     echo "no such model"
