f865059c2ef004f0767c30dd1f4cf99bac67fb17
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-11-11 18:58:08 +0100

    Fixed bad exp names

--- code/largeScale_hy_cub9.sh
+++ code/largeScale_hy_cub9.sh
@@ -1,21 +1,21 @@
 case $1 in
   "clus")
-    python trainVal.py -c model_cub9.config --exp_id CUB9_HYP --model_id clus   --resnet_bilinear True --bil_cluster True
+    python trainVal.py -c model_cub9.config --model_id clus   --resnet_bilinear True --bil_cluster True
     ;;
   "bil")
-    python trainVal.py -c model_cub9.config --exp_id CUB9_HYP --model_id bil    --resnet_bilinear True
+    python trainVal.py -c model_cub9.config --model_id bil    --resnet_bilinear True
     ;;
   "none")
     python trainVal.py -c model_cub9.config --model_id none
     ;;
   "clusRed")
-    python trainVal.py -c model_cub9.config --model_id clusRed     --resnet_bilinear True --bil_cluster True --stride_lay3 2 --stride_lay4 2 --max_batch_size 150
+    python trainVal.py -c model_cub9.config --model_id clusRed     --resnet_bilinear True --bil_cluster True --stride_lay3 2 --stride_lay4 2 --max_batch_size 130
     ;;
   "bilRed")
     python trainVal.py -c model_cub9.config --model_id bilRed      --resnet_bilinear True   --stride_lay3 2 --stride_lay4 2 --max_batch_size 130
     ;;
   "noneRed")
-    python trainVal.py -c model_cub9.config --model_id noneRed      --stride_lay3 2 --stride_lay4 2 --max_batch_size 150
+    python trainVal.py -c model_cub9.config --model_id noneRed      --stride_lay3 2 --stride_lay4 2 --max_batch_size 130
     ;;
   "*")
     echo "no such model"
