a947ee129dc4b1c405f846c2af6e2ed5871e2e7e
E144069X <tristan.gomez44@etu.univ-nantes.fr>
2020-11-13 10:07:14 +0100

    Fixed too big validation size

--- code/load_data.py
+++ code/load_data.py
@@ -139,7 +139,7 @@ def buildTestLoader(args, mode,shuffle=False,withSeg=False,reprVec=False):
         sampler=None
 
     if args.val_batch_size == -1:
-        args.val_batch_size = int(args.max_batch_size*4)
+        args.val_batch_size = int(args.max_batch_size*3.5)
 
     testLoader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=args.val_batch_size,
                                              num_workers=args.num_workers,sampler=sampler)
