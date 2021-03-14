de23a16dbbe1d251e20fb21ebfd75ad374ab05c3
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2021-03-14 16:49:54 +0100

    Added efficient net

--- code/models/efficientnet.py
+++ code/models/efficientnet.py
@@ -1,21 +1,26 @@
 from efficientnet_pytorch import EfficientNet
 import torch
 
-class EffNetB2(torch.nn.Module):
+class EffNetB4(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
 
-        self.model = EfficientNet.from_pretrained('efficientnet-b2')
+        self.model = EfficientNet.from_pretrained('efficientnet-b4')
 
     def forward(self,x):
         feat = self.model.extract_features(x)
         return {"x":feat}
 
 
-def efficientnet_b2():
-    model = EffNetB2()
+def efficientnet_b4():
+    model = EffNetB4()
     return model
 
 if __name__ == "__main__":
-    efficientnet_b2()
+    model = efficientnet_b4()
+
+    inp = torch.zeros(1,3,224,224)
+
+    out = model(inp)
+    print(out["x"].size())
