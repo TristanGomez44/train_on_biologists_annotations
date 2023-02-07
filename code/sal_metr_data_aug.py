02113e104dd172045211d5a5d9082d15b9e64b95
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-02-07 09:18:06 +0100

    Added arg to set probability of masking

--- code/sal_metr_data_aug.py
+++ code/sal_metr_data_aug.py
@@ -16,7 +16,7 @@ def get_att_maps(retDict):
 
     return attMaps
 
-def apply_att_metr_masks(model,data):
+def apply_sal_metr_masks(model,data,mask_prob=1):
 
     with torch.no_grad():
         retDict = model(data)
@@ -24,32 +24,35 @@ def apply_att_metr_masks(model,data):
     data_masked_list = []
     expl = get_att_maps(retDict)
     for i in range(len(data)):
-        metric_ind = torch.randint(0,len(metric_list),size=(1,)).item()
-        metric_name = metric_list[metric_ind]
-        if is_multi_step[metric_name]:
-            metric = metric_dic[metric_name]()
+        if torch.rand(size=(1,)).item() <= mask_prob:
+            metric_ind = torch.randint(0,len(metric_list),size=(1,)).item()
+            metric_name = metric_list[metric_ind]
+            if is_multi_step[metric_name]:
+                metric = metric_dic[metric_name]()
 
-            data_i = data[i:i+1]
-            masking_data_i = metric.get_masking_data(data_i)
-            expl_i = expl[i:i+1]
+                data_i = data[i:i+1]
+                masking_data_i = metric.get_masking_data(data_i)
+                expl_i = expl[i:i+1]
 
-            dic = metric.choose_data_order(data_i,masking_data_i)
-            data1,data2 = dic["data1"],dic["data2"]
+                dic = metric.choose_data_order(data_i,masking_data_i)
+                data1,data2 = dic["data1"],dic["data2"]
 
-            k = torch.randint(0,expl.shape[2]*expl.shape[3],size=(1,)).item()
-            mask,_ = metric.compute_mask(expl_i,data.shape,k)
-            data_masked = metric.apply_mask(data1,data2,mask)
+                k = torch.randint(0,expl.shape[2]*expl.shape[3],size=(1,)).item()
+                mask,_ = metric.compute_mask(expl_i,data.shape,k)
+                data_masked = metric.apply_mask(data1,data2,mask)
+                data_masked_list.append(data_masked)
+            else:
+                metric = metric_dic[metric_name]()
+
+                data_i = data[i:i+1]
+                masking_data_i = metric.get_masking_data(data_i)
+
+                mask = metric.compute_mask(expl[i:i+1],data.shape)
+                data_masked = metric.apply_mask(data_i,masking_data_i,mask)
+                data_masked_list.append(data_masked)
         else:
-            metric = metric_dic[metric_name]()
+            data_masked_list.append(data[i:i+1])
 
-            data_i = data[i:i+1]
-            masking_data_i = metric.get_masking_data(data_i)
-
-            mask = metric.compute_mask(expl[i:i+1],data.shape)
-            data_masked = metric.apply_mask(data_i,masking_data_i,mask)
-
-        data_masked_list.append(data_masked)
-    
     data_masked = torch.cat(data_masked_list,dim=0).to(data)
 
     return data_masked
