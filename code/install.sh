d2705bb24a22fdff250d849eeb62a35437b3fa2a
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-03-31 08:48:22 +0200

    Added install script

---
+++ code/install.sh
@@ -0,0 +1,4 @@
+conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
+conda install -y -c conda-forge matplotlib optuna opencv 
+conda install -y  -c anaconda scipy scikit-learn 
+pip3 install saliency_maps_metrics --upgrade --no-cache-dir
