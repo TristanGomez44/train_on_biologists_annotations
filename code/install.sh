034d862cb789c594aa4c88c083eca15d6e415c9f
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-04-12 14:57:17 +0200

    Replaced manual script with yml file

--- code/install.sh
+++
@@ -1,5 +0,0 @@
-conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
-conda install -y -c conda-forge matplotlib optuna opencv 
-conda install -y  -c anaconda scipy scikit-learn 
-conda install -y -c pytorch captum 
-pip3 install saliency_maps_metrics --upgrade --no-cache-dir
