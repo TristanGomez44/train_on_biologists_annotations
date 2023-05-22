9a2cc1f77b0cc426a4c45f90435129dd13bbfc5a
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-05-22 18:08:33 +0200

    Set batch size to train on single 10Go Gpu

--- code/grade.sh
+++ code/grade.sh
@@ -9,10 +9,10 @@ case $1 in
     python trainVal.py -c grade.config --model_id noneRed2_transf --epochs 100  --max_worse_epoch_nb 10 --first_mod vit_b_16  --big_images False --epochs 1 --debug True --start_mode scratch 
     ;;
   "noneRed2_lr_transf16")
-    python trainVal.py -c grade.config --model_id noneRed2_transf16 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_16 --big_images False --epochs 1
+    python trainVal.py -c grade.config --model_id noneRed2_transf16 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_16 --big_images False
     ;;
   "noneRed2_lr_transf8")
-    python trainVal.py -c grade.config --model_id noneRed2_transf8 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_8 --big_images False --epochs 1
+    python trainVal.py -c grade.config --model_id noneRed2_transf8 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_8 --big_images False --batch_size 8 --val_batch_size 75
     ;;
   "noneRed2_lr")
     python trainVal.py -c grade.config --model_id noneRed2_lr --epochs 100  --max_worse_epoch_nb 10 --first_mod resnet18 --big_images False --epochs 1
