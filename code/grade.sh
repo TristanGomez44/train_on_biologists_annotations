6732daa1ba8b9749cefca4864b2cdb95eebc3d27
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-05-23 14:11:12 +0200

    Removed useless debug and added author data aug model

--- code/grade.sh
+++ code/grade.sh
@@ -1,19 +1,11 @@
 case $1 in
-  "noneRed2_lr_swin_debug")
-    python trainVal.py -c grade.config --model_id noneRed2_swin --epochs 100  --max_worse_epoch_nb 10 --first_mod swin_b_16  --big_images False --epochs 1 --debug True  
-    ;;
   "noneRed2_lr_swin")
     echo swing
     python trainVal.py -c grade.config --model_id noneRed2_swin --epochs 100  --max_worse_epoch_nb 10 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128
     ;;
-  "noneRed2_lr_dino_debug")
-    python trainVal.py -c grade.config --model_id noneRed2_dino --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_16  --big_images False --epochs 1 --debug True  
-    ;;
-  "noneRed2_lr_dino8_debug")
-    python trainVal.py -c grade.config --model_id noneRed2_dino8 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_8  --big_images False --epochs 1 --debug True  
-    ;;
-  "noneRed2_lr_transf_debug")
-    python trainVal.py -c grade.config --model_id noneRed2_transf --epochs 100  --max_worse_epoch_nb 10 --first_mod vit_b_16  --big_images False --epochs 1 --debug True --start_mode scratch 
+  "noneRed2_lr_swin_authordataaug")
+    echo swing
+    python trainVal.py -c grade.config --model_id noneRed2_swin_authordataaug --epochs 100  --max_worse_epoch_nb 10 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128
     ;;
   "noneRed2_lr_transf16")
     python trainVal.py -c grade.config --model_id noneRed2_transf16 --epochs 100  --max_worse_epoch_nb 10 --first_mod dit_b_16 --big_images False
