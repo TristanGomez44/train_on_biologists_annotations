7af1243e8c55360ad15a0beb9eaa3460e25d20e7
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-06-01 14:10:18 +0200

    Added swin training with early stopping, ssl training on ccipl and log_gradient debug cmd

--- code/grade.sh
+++ code/grade.sh
@@ -5,8 +5,8 @@ case $1 in
   "noneRed2_lr_swin_reg_to_class")
     python trainVal.py -c grade.config --model_id noneRed2_swin_reg_to_class --epochs 100  --max_worse_epoch_nb 10 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --regression_to_classif True
     ;;
-  "noneRed2_lr_swin2")
-    python trainVal.py -c grade.config --model_id noneRed2_swin2  --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128
+  "noneRed2_lr_swin2_worse10")
+    python trainVal.py -c grade.config --model_id noneRed2_swin2_worse10 --epochs 100  --max_worse_epoch_nb 10 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --log_gradient_norm_frequ 1
     ;;
  "noneRed2_lr_swin_regression")
     python trainVal.py -c grade.config --model_id noneRed2_swin_regression  --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --regression True
@@ -18,10 +18,10 @@ case $1 in
     python trainVal.py -c grade.config --model_id noneRed2_r50 --first_mod resnet50 --big_images True --batch_size 64 --val_batch_size 512
     ;;
   "noneRed2_lr_swin_ssl")
-    python trainVal.py -c ssl.config --model_id noneRed2_swin --epochs 100 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --ssl True
+    python trainVal.py -c ssl.config --model_id noneRed2_swin_ssl --epochs 100 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --ssl True
     ;;
   "noneRed2_lr_swin_ssl_ccpl")
-    python trainVal.py -c ssl.config --model_id noneRed2_swin --epochs 100 --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --ssl True --dataset_path /LAB-DATA/LS2N/E144069X/DL4IVF/
+    python trainVal.py -c ssl.config --model_id noneRed2_swin_ssl --epochs 100 --first_mod swin_b_16  --big_images False --batch_size 32 --val_batch_size 64 --ssl True --dataset_path /LAB-DATA/LS2N/E144069X/DL4IVF/ --num_workers 2 --val_freq 1
     ;;
   "noneRed2_lr_swin_authordataaug_sched")
     python trainVal.py -c grade.config --model_id noneRed2_swin_authordataaug_sched --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --swa True
@@ -30,7 +30,7 @@ case $1 in
     python trainVal.py -c grade.config --model_id noneRed2_swin_one_feat_per_head_relu --first_mod swin_b_16  --big_images False --batch_size 64 --val_batch_size 128 --one_feat_per_head True
     ;;
   "debug")
-    python trainVal.py -c grade.config --model_id noneRed2_lr_r18 --epochs 15 --first_mod resnet18  --big_images False --start_mode scratch --one_feat_per_head True --regression_to_classif True
+    python trainVal.py -c grade.config --model_id noneRed2_lr_r18 --epochs 15 --first_mod resnet18  --big_images False --start_mode scratch --one_feat_per_head True --regression_to_classif True --log_gradient_norm_frequ 1
     ;;
   "debug_ssl")
     /home/E144069X/miniconda3/envs/myenv2/bin/python3 trainVal.py -c ssl.config --model_id noneRed2_lr_r18_ssl --epochs 15 --first_mod resnet18  --big_images False --start_mode scratch --one_feat_per_head True --ssl True --teach_temp_sched_epochs 3 --epochs 6 --dataset_path /media/E144069X/DL4IVF/DL4IVF/ --warmup_epochs 3 --end_cosine_sched_epoch 6
