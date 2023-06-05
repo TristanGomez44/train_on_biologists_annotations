21509f344931d486d0c1fa635e2340300a4dba51
E144069X <tristan.gomez@etu.univ-nantes.fr>
2023-06-05 17:43:42 +0200

    Removed scorecam

--- code/eval_expl_25.sh
+++ code/eval_expl_25.sh
@@ -7,7 +7,7 @@ fi
 
 if [ "$3" == "all" ];
 then
-    declare -a expl_list=("randommap" "topfeatmap" "randomfeatmap" "am" "cam" "gradcam" "gradcampp" "ablationcam" "ablationcamnous" "scorecam")
+    declare -a expl_list=("randommap" "topfeatmap" "randomfeatmap" "am" "cam" "gradcam" "gradcampp" "scorecam")
 else
     declare -a expl_list=($3)
 fi
