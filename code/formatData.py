4fcdab8b708cb9ffc017d7b83a4a2655d9a1ee5a
E144069X <E144069X@etu.univ-nantes.fr>
2023-11-06 15:48:32 +0100

    Addapted to database format

--- code/formatData.py
+++ code/formatData.py
@@ -3,6 +3,10 @@ import os,sys
 import zipfile
 import shutil
 import json
+import sqlite3
+import subprocess 
+from collections import defaultdict,Counter
+import math
 
 import numpy as np
 import pandas as pd
@@ -11,7 +15,75 @@ from args import ArgReader
 
 from grade_dataset import Tasks
 
-def format_dl4ivf_dataset(path_to_zip,path_to_annot_csv,dest_folder,train_prop,val_prop,fold_name="blastocyst_dataset",json_file_name="splits.json",seed=0):
+def get_task_and_value(annot):
+
+	assert len(annot) in [3,6],f"Annot has incorect length:{annot}"
+
+	pref,value = annot[:2],annot[2:]
+
+	if value == "Null":
+		value = "NaN"
+
+	assert pref in ["Tr","Bo","Ex","Na"],f"Inccorect annotation prefix:{pref}"
+
+	if pref =="Tr":
+		task = Tasks.TE
+	elif pref == "Bo":
+		task = Tasks.ICM
+	elif pref== "Ex":
+		task = Tasks.EXP
+	else:
+		task = None
+
+	return task,value
+
+def convert_db_to_csv(new_path_to_annot_file):
+
+	csv_filename = os.path.splitext(os.path.basename(new_path_to_annot_file))[0]+".csv"
+	csv_dirname = os.path.dirname(new_path_to_annot_file)
+	csv_path = os.path.join(csv_dirname,csv_filename)
+
+	queries = ["sqlite3",new_path_to_annot_file,".mode csv",".headers on",f".output {csv_path}","select nameImage,idTag from annotation join image on annotation.idImg=image.id"]
+
+	subprocess.call(queries)
+
+	return csv_path
+
+def aggregate_annotations(new_path_to_annot_file):
+	with open(new_path_to_annot_file) as file:
+		rows = [line.rstrip() for line in file]
+	aggr_dic = defaultdict(lambda:{task:[] for task in Tasks})
+	aggr_annot = []
+	for row in rows[1:]:
+		image_name,annot = row.split(",")
+		task,value = get_task_and_value(annot)
+		if task is not None:
+			aggr_dic[image_name][task].append(value)
+	for image_name in aggr_dic:
+		csv_row = [image_name]
+		for task in Tasks:
+			annot_list = aggr_dic[image_name][task]
+			annot_list = list(filter(lambda x:x!="NaN",annot_list))
+			if len(annot_list)==0:
+				csv_row.append("nan")
+			else:
+				counter = Counter(annot_list)
+				csv_row.append(counter.most_common(1)[0][0])
+		aggr_annot.append(csv_row)
+
+	return aggr_annot
+
+def format_annotations(path_to_annot_file,dest_folder):
+	annot_file_name = os.path.basename(path_to_annot_file)
+	new_path_to_annot_file = os.path.join(dest_folder,annot_file_name)
+	shutil.copy(path_to_annot_file,new_path_to_annot_file)
+	new_path_to_annot_file = convert_db_to_csv(new_path_to_annot_file)	
+	aggr_annot = aggregate_annotations(new_path_to_annot_file)
+	path_to_aggr_annot_csv = os.path.join(dest_folder,"aggregated_annotations.csv")
+	np.savetxt(path_to_aggr_annot_csv,aggr_annot,"%s")
+	return aggr_annot
+
+def format_dl4ivf_dataset(path_to_zip,path_to_annot_file,dest_folder,train_prop,val_prop,fold_name="blastocyst_dataset",json_file_name="splits.json",seed=0):
 
 	os.makedirs(dest_folder,exist_ok=True)
 
@@ -27,41 +99,20 @@ def format_dl4ivf_dataset(path_to_zip,path_to_annot_csv,dest_folder,train_prop,v
 	if not os.path.exists(new_img_folder_path):
 		os.rename(img_folder_path,new_img_folder_path)
 
-	csv_file_name = os.path.splitext(os.path.basename(path_to_annot_csv))[0]
+	aggr_annot = format_annotations(path_to_annot_file,dest_folder)
+	headers = ["image_name"]+[task.value for task in Tasks]
 
-	new_path_to_annot_csv = os.path.join(dest_folder,csv_file_name)
-	shutil.copy(path_to_annot_csv,new_path_to_annot_csv)
-
-	annotation_df = pd.read_csv(new_path_to_annot_csv)
-	grouped = annotation_df.groupby('image_name')
-
-	all_aggr_annot = None
-	for task in Tasks:
-		task_name = task.value
-		aggr_annotations = grouped[task_name].apply(lambda x: x.mode().iloc[0])
-		aggr_annotations = aggr_annotations.reset_index()
-
-		if all_aggr_annot is None:
-			all_aggr_annot = aggr_annotations
-		else:
-			all_aggr_annot[task_name] = aggr_annotations[task_name]
-	
-	path_to_aggr_annot_csv = os.path.join(dest_folder,"aggregated_annotations.csv")
-
-	all_aggr_annot.to_csv(path_to_aggr_annot_csv,index=False)
-	
-	splits = make_split(all_aggr_annot,train_prop,val_prop,seed)
+	splits = make_split(aggr_annot,headers,train_prop,val_prop,seed)
 
 	json_file_path = os.path.join(dest_folder,json_file_name)
 	with open(json_file_path, 'w') as fp:
 		json.dump(splits, fp)
 
-def make_split(all_aggr_annot,train_prop,val_prop=None,seed=0):
+def make_split(all_aggr_annot,headers,train_prop,val_prop=None,seed=0):
 
+	all_aggr_annot = pd.DataFrame(all_aggr_annot,columns=headers)
 	groups = all_aggr_annot.groupby([task.value for task in Tasks])
-	
 	np.random.seed(seed)
-
 	train_set,val_set,test_set = [],[],[]
 
 	for (_,sub_df) in groups:
@@ -150,6 +201,8 @@ def main(argv=None):
 	args = argreader.args
 
 	assert args.format_dl4ivf_dataset or args.format_multicenter_dataset,"Choose one of the two options"
+
+	assert os.path.splitext(args.path_to_dl4ivf_annot)[1] == ".db",'Annotations should be in database format'
 
 	if args.format_dl4ivf_dataset:
 		format_dl4ivf_dataset(args.path_to_zip,args.path_to_dl4ivf_annot,args.dest_folder,args.train_prop,args.val_prop)
