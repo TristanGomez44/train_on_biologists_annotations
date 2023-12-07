34003a35ac98c33fa852a4c462907a1373cdaa8c
E144069X <E144069X@etu.univ-nantes.fr>
2023-12-07 15:03:20 +0100

    Refactor

--- code/find_closest_neighbors.py
+++ code/find_closest_neighbors.py
@@ -1,85 +1,37 @@
 
 import os
-import sys
-import glob
-
-from shutil import copyfile
-import gc
-import subprocess
 
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
-plt.switch_backend('agg')
-import optuna
-import sqlite3
 import cv2
+import torch.nn.functional as F
 
-from utils import _remove_no_annot
-from args import ArgReader,str2bool,addInitArgs,addValArgs,init_post_hoc_arg,addLossTermArgs,addSalMetrArgs
-import init_model
-from loss import SupervisedLoss,SelfSuperVisedLoss,agregate_losses
+from args import ArgReader,addValArgs,init_post_hoc_arg,addSalMetrArgs
 import modelBuilder
 import load_data
-import metrics
-import update
 import utils 
 import trainVal
+from utils import get_feature_and_output,get_datasets_and_model
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
-def get_feature_and_output(train_loader,test_loader,net,args):
-    result_dic = {}
-    for label,loader in zip(["train","test"],[train_loader,test_loader]):
-        pooled_features_path = f"../results/{args.exp_id}/pooled_features_{args.model_id}_{label}.npy"
-        
-        result_dic[label] = {}
-        if not os.path.exists(pooled_features_path):
-            print(f"Inference on {label} set")
-            pooled_features = []
-            features = []
-            output = []
-            target = []
-            for idx,batch in enumerate(loader):
-        
-                if idx%10 == 0:
-                    print(f"\tProcessing batch {idx}/{len(loader)}")
-
-                image,annot_dict = batch
-                output_dic = net(image)        
-            
-                pooled_features.append(output_dic["feat_pooled"].cpu().numpy())
-                features.append(output_dic["feat"].cpu().numpy())
-                output.append(output_dic["output_"+args.task].cpu().numpy())
-                target.append(annot_dict[args.task].cpu().numpy())
-
-                if args.debug:
-                    break
-
-            pooled_features = np.concatenate(pooled_features,axis=0)
-            features = np.concatenate(features,axis=0)
-            output = np.concatenate(output,axis=0)
-            target = np.concatenate(target,axis=0)
-
-            np.save(pooled_features_path,pooled_features)
-            np.save(pooled_features_path.replace("pooled_features","features"),features)
-            np.save(pooled_features_path.replace("pooled_features","output"),output)
-            np.save(pooled_features_path.replace("pooled_features","target"),target)
-
-        else:
-            pooled_features = np.load(pooled_features_path)
-            features = np.load(pooled_features_path.replace("pooled_features","features"))
-            output = np.load(pooled_features_path.replace("pooled_features","output"))
-            target = np.load(pooled_features_path.replace("pooled_features","target"))
-
-        result_dic[label]["pooled_features"] = pooled_features
-        result_dic[label]["features"] = features
-        result_dic[label]["output"] = output
-        result_dic[label]["target"] = target
-    
-    return result_dic
+def cos_sim(a,b):
+    #a = a[:,None,:]
+    #b = b[None,:,:]
+    a = torch.tensor(a)
+    b = torch.tensor(b)
+    print("cosim",a.shape,b.shape)
+    C = F.normalize(a) @ F.normalize(b).t()
+    print("cosim",C.shape)
+    return C
+    #print("cosim",a.shape,b.shape)
+    #return torch.nn.functional.cosine_similarity(a,b,axis=axis).numpy()
 
 def find_closest_neighbor(result_dic,args):
 
     closest_train_idx_path = f"../results/{args.exp_id}/closest_train_idx_{args.model_id}.npy"
+
+    print(closest_train_idx_path)
 
     if not os.path.exists(closest_train_idx_path):
 
@@ -93,7 +45,8 @@ def find_closest_neighbor(result_dic,args):
             train_idx = np.where(result_dic["train"]["target"] == label)[0]
             train_pooled_features = result_dic["train"]["pooled_features"][train_idx]
 
-            dist = np.linalg.norm(test_pooled_features[:,None,:] - train_pooled_features[None,:,:],axis=-1)
+            print(result_dic["test"]["pooled_features"][0].shape,test_idx,result_dic["train"]["pooled_features"][0].shape,train_idx)
+            dist = -cos_sim(test_pooled_features,train_pooled_features)
 
             closest_train_idx.extend(train_idx[np.argmin(dist,axis=-1)])
 
@@ -101,11 +54,11 @@ def find_closest_neighbor(result_dic,args):
         np.save(closest_train_idx_path,closest_train_idx)
             
     else:
-        closest_train_idx = np.load(closest_train_idx_path)
+        closest_train_idx = np.load(closest_train_idx_path,allow_pickle=True)
 
     return closest_train_idx
 
-def similarity_map(train_feat,test_feat,maps_shape):
+def similarity_map(train_feat,test_feat,maps_shape,sim_map="mean_activation"):
 
     train_feat = train_feat.transpose()
     test_feat = test_feat.transpose()
@@ -113,13 +66,18 @@ def similarity_map(train_feat,test_feat,maps_shape):
     train_feat = train_feat[:,None,:]
     test_feat = test_feat[None,:,:]
 
-    #cosine similarity between the most active feature and the other features
-    #sim_matrix = (train_feat*test_feat).sum(axis=-1)/(np.linalg.norm(train_feat,axis=-1)*np.linalg.norm(test_feat,axis=-1))
-    sim_matrix = (train_feat*test_feat).sum(axis=-1)
+    if sim_map == "dot":
+        sim_matrix = (train_feat*test_feat).sum(axis=-1)
+        train_sim_map = sim_matrix.max(axis=-1)
+        test_sim_map = sim_matrix.max(axis=0)
+    elif sim_map == "mean_activation":
+        train_sim_map = train_feat.mean(axis=-1)
+        print("mean_act",train_feat.shape,train_sim_map.shape)
+        test_sim_map = test_feat.mean(axis=-1)
+    else:
+        raise ValueError("Unkown sim map method",sim_map)
 
-    train_sim_map = sim_matrix.max(axis=-1)
-    test_sim_map = sim_matrix.max(axis=0)
-
+    print(train_sim_map.shape)
     train_sim_map = train_sim_map.reshape(maps_shape)
     test_sim_map = test_sim_map.reshape(maps_shape)
 
@@ -141,13 +99,18 @@ def similarity_maps(args,result_dic,closest_train_idx):
             test_feat = result_dic["test"]["features"][i]
             train_feat = result_dic["train"]["features"][closest_train_idx[i]]
             
+            print("similarity_maps() test_feat",test_feat.shape,train_feat.shape,closest_train_idx[i])
+
             #Stores maps shape 
             maps_shape = test_feat.shape[1:]
-
-            test_feat = test_feat.reshape(test_feat.shape[0],-1)
+            #print("ORIGINAL SHAPE",test_feat.shape)
+            old_shape = train_feat.shape
             train_feat = train_feat.reshape(train_feat.shape[0],-1)
+            
+            test_feat = test_feat.reshape(test_feat.shape[0],-1)
 
-            train_map,test_map= similarity_map(train_feat,test_feat,maps_shape)
+            print("train_feat",train_feat.shape,"test_feat",test_feat.shape)
+            train_map,test_map = similarity_map(train_feat,test_feat,maps_shape)
 
             train_maps.append(train_map)
             test_maps.append(test_map)
@@ -166,8 +129,9 @@ def similarity_maps(args,result_dic,closest_train_idx):
 
 def preproc_map(img,sal_map,cmap):
     sal_map = utils.normalize_tensor(sal_map)
+
     sal_map = cmap(sal_map)[:,:,:3]
-    sal_map = cv2.resize(sal_map, (img.shape[2], img.shape[1]),interpolation = cv2.INTER_NEAREST).astype(np.float32)
+    sal_map = cv2.resize(sal_map, (img.shape[2], img.shape[1]),interpolation = cv2.INTER_CUBIC).astype(np.float32)
     sal_map = sal_map.transpose(2,0,1)
     img = img.mean(axis=0,keepdim=True).expand(3,-1,-1)  
     sal_map = 0.2*img + 0.8*sal_map
@@ -183,9 +147,6 @@ def show_similarity_map(args,test_dataset,train_dataset,closest_train_idx,train_
 
     for i in range(len(closest_train_idx)):
 
-        if i % 20 == 0:
-            print(f"{i}/{len(closest_train_idx)}")
-
         test_idx = i
         train_idx = closest_train_idx[i]
 
@@ -199,16 +160,21 @@ def show_similarity_map(args,test_dataset,train_dataset,closest_train_idx,train_
         test_map = preproc_map(test_img,test_maps[i],cmap)
 
         grid.append(test_img)
-        grid.append(train_map)
-        grid.append(train_img)
         grid.append(test_map)
+        grid.append(train_img)
+        grid.append(train_map)
     
         if args.debug and i==5:
             break
+        
+        if i % 5 == 0:
+            print(f"{i}/{len(closest_train_idx)}")
 
-    grid = torch.from_numpy(np.stack(grid,0))
-    fig_path = f"../vis/{args.exp_id}/similarity_map_{args.model_id}.png"
-    utils.save_image(grid,fig_path,row_nb=len(grid)//4,apply_inv_norm=False)
+            grid = torch.from_numpy(np.stack(grid,0))
+            fig_path = f"../vis/{args.exp_id}/similarity_map_{args.model_id}_{i}.png"
+            utils.save_image(grid,fig_path,row_nb=len(grid)//4,apply_inv_norm=False)
+
+            grid = []
 
 def main(argv=None):
     # Getting arguments from config file and command line
@@ -225,23 +191,15 @@ def main(argv=None):
     argreader = addSalMetrArgs(argreader)
     argreader = init_post_hoc_arg(argreader)
 
-    argreader.parser.add_argument('--task', type=str, default="icm")
+    argreader.parser.add_argument('--task_to_train', type=str, default="all")
+    argreader.parser.add_argument('--task_output_to_save', type=str, default="ICM")
 
     # Reading the comand line arg
     argreader.getRemainingArgs()
 
     args = argreader.args
 
-    train_loader,train_dataset = load_data.buildTestLoader(args,"train")
-    test_loader,test_dataset = load_data.buildTestLoader(args,"test")
-
-    net = modelBuilder.netBuilder(args)
-    best_paths = glob.glob(f"../models/{args.exp_id}/model{args.model_id}_best_epoch*")
-    assert len(best_paths) == 1, "There should be only one best model"
-    best_path = best_paths[0]
-    net = init_model.preprocessAndLoadParams(best_path,args.cuda,net,ssl=args.ssl)
-    net.eval()
-    torch.set_grad_enabled(False)
+    train_loader,train_dataset,test_loader,test_dataset,net = get_datasets_and_model(args)
 
     result_dic = get_feature_and_output(train_loader,test_loader,net,args)
 
